62e00a61839c350392805d0b98dc72da84ca0fa0
ayubkhan4gcp <160510136+ayubkhan4gcp@users.noreply.github.com>
2024-03-06 10:19:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,12 @@ variable "bucket_name" {
   type    = string
   default = ""
 }
+variable "terraform_service_account" {
+  type    = string
+  default = ""
+}
+
+variable "project_id" {
+  type    = string
+  default = ""
+}

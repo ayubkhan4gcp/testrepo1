1f571310b8b1127ca80e90384d58fa088f58aabb
ayubkhan4gcp <160510136+ayubkhan4gcp@users.noreply.github.com>
2024-03-11 20:12:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,5 +3,8 @@ terraform {
     bucket = "terraform_state_bucket7417"
     prefix = "terraform/state"
     skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
   }
 }
